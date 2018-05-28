a207877b1469e0eed1806538f927d0cc8d249797
brel-baz <brel-baz@student.42.fr>
2018-05-28 23:20:03 +0200

    ex02 d01 test a finir

--- D01/ex02/Deck.swift
+++ D01/ex02/Deck.swift
@@ -1,6 +1,6 @@
 class Deck: NSObject {
-    static let allHeart: [Card] = Value.allValues.map{Card(c: Color.Heart, v: $0)};
+    static let allHearts: [Card] = Value.allValues.map{Card(c: Color.Heart, v: $0)};
     static let allSpades: [Card] = Value.allValues.map{Card(c: Color.Spade, v: $0)};
-    static let allDiamond: [Card] = Value.allValues.map{Card(c: Color.Diamond, v: $0)};
-    static let allClub: [Card] = Value.allValues.map{Card(c: Color.Club, v: $0)};
+    static let allDiamonds: [Card] = Value.allValues.map{Card(c: Color.Diamond, v: $0)};
+    static let allClubs: [Card] = Value.allValues.map{Card(c: Color.Club, v: $0)};
 }
---
+++ D01/ex02/Main.swift
@@ -0,0 +1,29 @@
+//for col in Color.allColors{
+//    print("\(col): \(col.rawValue) | type = ",type(of: col.rawValue));
+//    // print(type(of: col.rawValue))
+//}
+//
+//for val in Value.allValues{
+//    print("\(val): \(val.rawValue) | type = ",type(of: val.rawValue))
+//}
+//
+//print("\u{001B}[1;32m\n- Cards\u{001B}[0m\n");
+//let card1 = Card(c : Color.Spade, v : Value.Ace);
+//let card2 = Card(c : Color.Spade, v : Value.Ace);
+//let card3 = Card(c : Color.Diamond, v : Value.Two);
+//print("   Expected   |   Returned");
+//print("--------------|--------------")
+//print("  (1, Spade)  |  \(card1)");
+//print("  (1, Spade)  |  \(card2)");
+//print(" (2, Diamond) | \(card3)");
+//print("     true     |     \(card1.isEqual(object: card2))");
+//print("    false     |     \(card1.isEqual(object: card3))");
+//print("     true     |     \(card1 == card2)");
+//print("    false     |     \(card1 == card3)");
+
+//print("\u{001B}[1;32m\n- Deck\u{001B}[0m\n");
+print("\(Deck.allSpades.count) Spades   : \(Deck.allSpades)");
+print("\(Deck.allClubs.count) Clubs    : \(Deck.allClubs)");
+print("\(Deck.allDiamonds.count) Diamonds : \(Deck.allDiamonds)");
+print("\(Deck.allHearts.count) Hearts   : \(Deck.allHearts)\n");
+print("\(Deck.allCards.count) Cards    : \(Deck.allCards)");
